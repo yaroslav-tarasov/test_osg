e1ae7c8aaae2017a48b6273da0179517d5da8b95
Yaroslav Tarasov <yaroslav.tarasov@gmail.com>
2015-04-14 22:23:11 +0300

    Переименование

--- utils/conv.ps1
+++ utils/conv.ps1
@@ -6,5 +6,5 @@ $source = Get-ChildItem -Path $data_path -Filter "*"
 $cmd  = $env:simex_dir + "\bin\release\conv.exe  "
 $source |% { & $cmd @( ($data_path + "\" + $_.name  + "\" + $_.name  +".dae")  , ($data_path + "\" + $_.name + "\" + $_.name + ".osgb") ) }
 
-$cmd2  = $env:simex_dir + "\bin\release\daeTest.exe  "
+$cmd2  = $env:simex_dir + "\bin\release\daextr.exe  "
 $source |% { & $cmd2 @( ($data_path + "\" + $_.name  + "\" + $_.name  +".dae") ) }
